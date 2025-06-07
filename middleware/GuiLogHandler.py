2b07f84058b36fdd52f9f4a2e1afe773ebb6069c
Shiirororo <chacchankhongphainhan313@gmail.com>
2025-06-07 16:57:31 +0700

    Update

--- middleware/GuiLogHandler.py
+++ middleware/GuiLogHandler.py
@@ -8,9 +8,26 @@ class GuiLogHandler(logging.Handler):
 
     def emit(self, record):
         msg = self.format(record)
+
         def append():
-            self.text_widget.configure(state='normal')
-            self.text_widget.insert(tk.END, msg + '\n')
-            self.text_widget.see(tk.END)
-            self.text_widget.configure(state='disabled')
-        self.text_widget.after(0, append)
+            # Kiểm tra widget còn tồn tại và chưa bị hủy
+            if self.text_widget is None:
+                return
+            if not self.text_widget.winfo_exists():
+                return
+            try:
+                self.text_widget.configure(state='normal')
+                self.text_widget.insert(tk.END, msg + '\n')
+                self.text_widget.see(tk.END)
+                self.text_widget.configure(state='disabled')
+            except tk.TclError:
+                pass  # Widget đã bị destroy
+
+        try:
+            if self.text_widget is not None and self.text_widget.winfo_exists():
+                self.text_widget.after(0, append)
+        except tk.TclError:
+            pass
+    def destroy(self):
+        self.text_widget = None
+        self.setLevel(logging.NOTSET)
